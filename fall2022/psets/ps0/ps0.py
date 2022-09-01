80e056e24236b22413ff920d5fba24cde8772f5d
Chris Partridge <cpartridge@college.harvard.edu>
2022-08-31 22:13:58 -0400

    ps0 thru 1c

--- fall2022/psets/ps0/ps0.py
+++ fall2022/psets/ps0/ps0.py
@@ -4,7 +4,8 @@
 #               #
 #################
 
-
+# Collaboration statement
+#   worked with Sofia Lysenko and Anne Foley on this pset.
 #
 # Setup
 #
@@ -30,8 +31,21 @@ class BTvertex:
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
 def calculate_sizes(v):
-    # Your code goes here
-    pass
+    # Top down approach (maybe bottom up?)
+    if (v.left == None) & (v.right == None):
+        v.size = 1;
+        return 1;
+    elif (v.left != None) & (v.right == None):
+        v.size = 1 + calculate_sizes(v.left)
+    elif (v.left == None) & (v.right != None):
+        v.size = 1 + calculate_sizes(v.right)       
+    else:
+        # print(calculate_sizes(v.left))
+        leftSize = int(calculate_sizes(v.left))
+        rightSize = int(calculate_sizes(v.right))
+        v.size = 1 + leftSize + rightSize
+        return v.size
+
 
 #
 # Problem 1c
@@ -43,5 +57,36 @@ def calculate_sizes(v):
 # ... in disjoint trees that all have at most n/2 vertices
 # Runtime: O(h)
 def find_vertex(r): 
-    # Your code goes here
-    pass
+    # print(r.size)
+    target = int((r.size)/2)
+    # print(target)
+    if (r == None) | (r.size == target) | ((r.left == None) & (r.right == None)):
+        return r
+    # only traverse to children 
+    # index vertex
+    c = r
+    while(True):
+        if (c.left != None) and (c.right != None):
+            if (c.left.size <= target) and (c.right.size <= target):
+                ans = c
+                break
+
+            if c.left.size > c.right.size:
+                c = c.left
+            elif c.left.size < c.right.size: 
+                c = c.right
+        elif c.left == None and c.right != None:
+            c = c.right
+        elif c.left != None and c.right == None:
+            c = c.left
+        elif c.size != None and c.size <= target:
+            ans = c
+            break
+
+    return ans
+    # keep array of disjoint tree sizes for every potential node
+    # how to traverse tree? recursively? for loop?
+
+    # For parent disj tree, their new size is current size minus current vertex size
+    # for child disj tree, size is that size
+    
