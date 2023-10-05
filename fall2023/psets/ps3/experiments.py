43e2e8afaf3e83a212bf66e8c5980b0847691c9e
bjpchoi <90070718+bjpchoi@users.noreply.github.com>
2023-10-04 22:45:15 -0400

    Update experiments.py

--- fall2023/psets/ps3/experiments.py
+++ fall2023/psets/ps3/experiments.py
@@ -27,21 +27,22 @@ seventeen_id = 7
 temp_id = 8   # Used only in prog2
 W_id = 9       # Used only in prog2
 
-# TODO: Fill in prog1 with the first RAM program provided in the homework.
 prog1 = [8, 
     ['assign', zero_id, 0],
     ['assign', one_id, 1], 
     ['assign', seventeen_id, 17],
     ['assign', output_len_id, 1], 
     ['assign', output_ptr_id, 0],
-    # TODO: lines 5-8 from pseudocode
+    ['assign', result_id, 17],
+    ['read', counter_id, zero_id],
+    ['goto', counter_id, 11],
+    ['*', result_id, result_id, result_id],
     ['-', counter_id, counter_id, one_id],
     ['goto', zero_id, 7],
     ['*', result_id, result_id, seventeen_id],
     ['write', output_ptr_id, result_id]
 ]
 
-# TODO: Fill in prog2 with the second RAM program provided in the homework.
 prog2 = [10, 
     ['assign', zero_id, 0],
     ['assign', one_id, 1], 
@@ -57,7 +58,12 @@ prog2 = [10,
     ['*', temp_id, temp_id, W_id],
     ['-', result_id, result_id, temp_id],
     ['-', counter_id, counter_id, one_id],
-    # TODO: lines 14-19 from pseudocode
+    ['goto', zero_id, 8],
+    ['*', result_id, result_id, seventeen_id],
+    ['/', temp_id, result_id, W_id],
+    ['*', temp_id, temp_id, W_id],
+    ['-', result_id, result_id, temp_id],
+    ['write', output_ptr_id, result_id]
 ]
 
 
